746860768a175894ed9015f734e7d4fd49753083
John Bellizzi <jbellizzi@axisgroup.com>
2020-02-24 14:50:09 -0500

    promoting README to dist

--- scripts/prepare-package.js
+++ scripts/prepare-package.js
@@ -14,53 +14,6 @@ fs.writeFile(path.join(__dirname, "../dist/package.json"), JSON.stringify(modPkg
   if (err) console.log(err)
 })
 
-// // Qlik Class Entrypoints
-// const classes = [
-//   "Doc",
-//   "Field",
-//   "GenericBookmark",
-//   "GenericDerivedFields",
-//   "GenericDimension",
-//   "GenericMeasure",
-//   "GenericObject",
-//   "GenericVariable",
-//   "Global",
-//   "Variable"
-// ];
-// classes.forEach(qClass => {
-//   var operatorFolder = path.join(__dirname, "../src", qClass);
-//   var operatorOutputFolder = path.join(__dirname, "../dist", qClass);
-//   mkDir(operatorOutputFolder);
-
-//   fs.readdir(operatorFolder, (err, files) => {
-//     if (err) return console.log(err);
-//     createPackage("index.js", qClass, operatorOutputFolder, 1);
-//   });
-// });
-
-// function createPackage(filename, folderPath, outputFolder, depth) {
-//   var trail = "../".repeat(depth);
-
-//   var pkg = {
-//     main: `${trail}_cjs/${folderPath}/${filename}`,
-//     module: `${trail}_esm5/${folderPath}/${filename}`,
-//     es2015: `${trail}_esm2015/${folderPath}/${filename}`,
-//     sideEffects: false
-//   };
-
-//   mkDir(outputFolder);
-
-//   fs.writeFile(
-//     path.join(outputFolder, "package.json"),
-//     JSON.stringify(pkg, null, "\t"),
-//     function(err) {
-//       if (err) console.log(err);
-//     }
-//   );
-// }
-
-// function mkDir(dir) {
-//   if (!fs.existsSync(dir)) {
-//     fs.mkdirSync(dir);
-//   }
-// }
+fs.copyFile(path.join(__dirname, "../README.md"), path.join(__dirname, "../dist/README.md"), function(err) {
+  if (err) console.log(err)
+})
