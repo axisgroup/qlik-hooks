143c5a0bae7556c053236ffa4d13dc13e2161e6a
John Bellizzi <jbellizzi@axisgroup.com>
2020-05-27 13:35:59 -0400

    updating qlik core engine

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import { useQlikContext } from "./connect/QlikProvider"
 export { default as useConnectEngine } from "./connect/useConnectEngine"
 export { default as QlikProvider } from "./connect/QlikProvider"
 export { useQlikContext }
-export { default as schema } from "./schema/schema-12.612.0.json"
+export { default as schema } from "./schema/schema-12.657.0.json"
